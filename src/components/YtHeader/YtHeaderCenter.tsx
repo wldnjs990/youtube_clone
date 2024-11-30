ba9beb2e2f8c32a03b1f33c6003e0c912cd6da32
wl990@naver.com <wl990@naver.com>
2024-12-01 04:18:44 +0900

    헤더 검색창 반응형 구현

--- src/components/YtHeader/YtHeaderCenter.tsx
+++ src/components/YtHeader/YtHeaderCenter.tsx
@@ -2,8 +2,11 @@ export default function YtHeaderCenter() {
   return (
     <article className="flex-[0_1_732px] min-w-0">
       <form action="" className="flex items-center w-full pl-11 pr-1">
-        <label htmlFor="search" className="flex w-full h-[40px] px-1">
-          <div className="flex-1 flex items-center rounded-l-[50px] pl-4 pr-1 border border-[#cccccc] shadow-inner focus-within:border-blue-500">
+        <label
+          htmlFor="search"
+          className="flex w-full h-[40px] px-1 justify-end"
+        >
+          <div className="flex-1 flex items-center rounded-l-[50px] pl-4 pr-1 border border-[#cccccc] shadow-inner focus-within:border-blue-500 max-sm:hidden">
             <img
               src="/public/YtSearch.svg"
               alt="유튜브 검색 아이콘"
@@ -24,7 +27,7 @@ export default function YtHeaderCenter() {
               </span>
             </a>
           </div>
-          <button className="flex items-center justify-center rounded-r-[50px] border border-l-0 border-[#cccccc] w-[64px] px-[6px] py-[1px] bg-[#f2f2f2] hover:bg-[#e3e3e3]">
+          <button className="flex items-center justify-center rounded-r-[50px] border border-l-0 border-[#cccccc] w-[64px] px-[6px] py-[1px] bg-[#f2f2f2] hover:bg-[#e3e3e3] max-sm:rounded-full max-sm:border-none max-sm:w-[40px] max-sm:bg-white">
             <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24">
               <path
                 clipRule="evenodd"
@@ -35,7 +38,7 @@ export default function YtHeaderCenter() {
           </button>
         </label>
         <label htmlFor="speak_to_search" className="relative">
-          <button className="w-10 h-10 ml-3 rounded-full flex items-center justify-center bg-[#f2f2f2] hover:bg-[#e3e3e3]">
+          <button className="w-10 h-10 ml-3 rounded-full flex items-center justify-center bg-[#f2f2f2] hover:bg-[#e3e3e3] max-sm:bg-white max-sm:ml-0">
             <img src="YtVoice.svg" alt="유튜브 음성녹음 아이콘" />
           </button>
         </label>
